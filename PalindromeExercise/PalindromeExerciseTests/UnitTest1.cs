a47e4754d8f38edda413f231a70d77a292519344
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-26 15:05:27 -0400

    Exercise Complete

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData ("racecar", true)]
+        [InlineData ("rainbowcat", false)]
+        [InlineData ("tacocat", true)]
+        public void Palindrome(string testWord, bool expected)
         {
-
+            //Arrage
+            var wordSmith = new WordSmith();
+            //Act
+            var actual = wordSmith.IsAPalindrome(testWord);
+            //Assert
+            Assert.Equal(actual, expected);
         }
     }
 }
